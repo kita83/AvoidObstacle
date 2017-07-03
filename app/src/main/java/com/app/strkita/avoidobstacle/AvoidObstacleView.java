7946ff609a8cdd7a68f34ea4b02793ab9c8afb4c
strkita <kita83@gmail.com>
2017-07-03 21:07:48 +0900

    first commit

---
+++ app/src/main/java/com/app/strkita/avoidobstacle/AvoidObstacleView.java
@@ -0,0 +1,15 @@
+package com.app.strkita.avoidobstacle;
+
+import android.content.Context;
+import android.view.SurfaceView;
+
+/**
+ *
+ * Created by strkita on 2017/07/02.
+ */
+
+public class AvoidObstacleView extends SurfaceView {
+    public AvoidObstacleView(Context context) {
+        super(context);
+    }
+}
---
+++ app/src/main/java/com/app/strkita/avoidobstacle/Obstacle.java
@@ -0,0 +1,8 @@
+package com.app.strkita.avoidobstacle;
+
+/**
+ * Created by strkita on 2017/07/02.
+ */
+
+public class Obstacle {
+}
